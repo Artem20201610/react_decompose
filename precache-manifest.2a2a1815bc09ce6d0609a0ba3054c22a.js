c6e3a2998ae7e8409eec422f6ad9382956f811b4
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-12 15:26:48 +0300

    Updates

---
+++ precache-manifest.2a2a1815bc09ce6d0609a0ba3054c22a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e6614f01686dfc074623eca0f73757",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d34993b4f0ad23fdba8",
+    "url": "./static/css/main.2767da29.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d34993b4f0ad23fdba8",
+    "url": "./static/js/main.fd8b145e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
